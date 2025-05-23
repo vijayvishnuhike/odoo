bad844e72ce6577549d93ae2fca4100b4885ae81
Gurunathan S <gurunathan@mettlerhealth.com>
2025-05-23 13:44:16 +0530

    test

--- odoo/addons/base/models/res_users.py
+++ odoo/addons/base/models/res_users.py
@@ -306,23 +306,23 @@ class ResUsers(models.Model):
         pass
 
 
-# class ResUsersLog(models.Model):
-#     _name = 'res.users.log'
-#     _order = 'id desc'
-#     _description = 'Users Log'
-#     # Uses the magical fields `create_uid` and `create_date` for recording logins.
-#     # See `bus.presence` for more recent activity tracking purposes.
+class ResUsersLog(models.Model):
+    _name = 'res.users.log'
+    _order = 'id desc'
+    _description = 'Users Log'
+    # Uses the magical fields `create_uid` and `create_date` for recording logins.
+    # See `bus.presence` for more recent activity tracking purposes.
 
-#     @api.autovacuum
-#     def _gc_user_logs(self):
-#         self._cr.execute("""
-#             DELETE FROM res_users_log log1 WHERE EXISTS (
-#                 SELECT 1 FROM res_users_log log2
-#                 WHERE log1.create_uid = log2.create_uid
-#                 AND log1.create_date < log2.create_date
-#             )
-#         """)
-#         _logger.info("GC'd %d user log entries", self._cr.rowcount)
+    @api.autovacuum
+    def _gc_user_logs(self):
+        self._cr.execute("""
+            DELETE FROM res_users_log log1 WHERE EXISTS (
+                SELECT 1 FROM res_users_log log2
+                WHERE log1.create_uid = log2.create_uid
+                AND log1.create_date < log2.create_date
+            )
+        """)
+        _logger.info("GC'd %d user log entries", self._cr.rowcount)
 
 
 class Users(models.Model):
