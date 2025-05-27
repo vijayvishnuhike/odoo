e79294513f13cdcbb48f9d6f723d0ac93be66998
Gurunathan S <gurunathan@mettlerhealth.com>
2025-05-27 17:09:18 +0530

    test

--- odoo/http.py
+++ odoo/http.py
@@ -224,6 +224,7 @@ CSRF_TOKEN_SALT = 60 * 60 * 24 * 365
 DEFAULT_LANG = 'en_US'
 
 GEOIP_EMPTY_COUNTRY = type('GeoIPEmptyCountry', (), {'name': None})()
+GEOIP_EMPTY_CITY = type('GeoIPEmptyCity', (), {'name': None})()
 
 # The dictionary to initialise a new session with.
 def get_default_session():
