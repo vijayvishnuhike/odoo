b66c56c86b60b11d6bf4f8ea08a6a0d4adf7bb63
Gurunathan S <gurunathan@mettlerhealth.com>
2025-05-28 16:49:31 +0530

    test

--- addons/web/static/src/webclient/webclient.js
+++ addons/web/static/src/webclient/webclient.js
@@ -6,8 +6,8 @@ import { registry } from "@web/core/registry";
 import { useBus, useService } from "@web/core/utils/hooks";
 import { ActionContainer } from "./actions/action_container";
 import { NavBar } from "./navbar/navbar";
-// import './test.css'
-import "@web/webclient/test.css";
+import './test.css'
+// import "@web/webclient/test.css";
 
 
 import { Component, onMounted, onWillStart, useExternalListener, useState } from "@odoo/owl";
