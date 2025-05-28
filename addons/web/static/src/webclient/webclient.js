9526c28906f8c788bd2222ac15ae73ae8ea55ed8
Gurunathan S <gurunathan@mettlerhealth.com>
2025-05-28 14:01:59 +0530

    test

--- addons/web/static/src/webclient/webclient.js
+++ addons/web/static/src/webclient/webclient.js
@@ -6,7 +6,7 @@ import { registry } from "@web/core/registry";
 import { useBus, useService } from "@web/core/utils/hooks";
 import { ActionContainer } from "./actions/action_container";
 import { NavBar } from "./navbar/navbar";
-import './test.css'
+// import './test.css'
 // import "@web/webclient/test.css";
 
 
